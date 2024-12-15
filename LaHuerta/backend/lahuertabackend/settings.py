1a79fa79f25cd8176d9cbfadf579657fcdab3f5e
Pablo Antúnez <pablooantunez@hotmail.com>
2024-12-15 17:48:31 -0300

    Fix on Clientform

--- LaHuerta/backend/lahuertabackend/settings.py
+++ LaHuerta/backend/lahuertabackend/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LaHuerta',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'lampard8#',
         'HOST': 'localhost',
         'PORT': '3306',
     }
