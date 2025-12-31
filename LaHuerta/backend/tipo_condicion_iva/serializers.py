1148a7d2d9216402091460ad0c76b4563f5c91f2
Pablo Antúnez <pablooantunez@hotmail.com>
2025-12-31 19:14:26 -0300

    update: change views.py of type_condition_iva app

--- LaHuerta/backend/tipo_condicion_iva/serializers.py
+++ LaHuerta/backend/tipo_condicion_iva/serializers.py
@@ -2,9 +2,6 @@ from rest_framework import serializers
 from .models import TipoCondicionIva
 
 class ConditionIvaTypeSerializer(serializers.ModelSerializer):
-    '''
-    DTO
-    '''
     class Meta:
         model = TipoCondicionIva
         fields = ['id', 'descripcion']
