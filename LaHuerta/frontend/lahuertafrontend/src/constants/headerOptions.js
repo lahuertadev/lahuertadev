3f9b588d7af4f6b2c4e6d18052f4a1b3cb60e9ca
Pablo Antúnez <92406312+pabloantunez@users.noreply.github.com>
2026-02-09 19:10:34 -0300

    update: add category view (#13)

--- LaHuerta/frontend/lahuertafrontend/src/constants/headerOptions.js
+++ LaHuerta/frontend/lahuertafrontend/src/constants/headerOptions.js
@@ -1,8 +1,15 @@
-import { FaHome, FaUser, FaMoneyBill, FaFileInvoice } from 'react-icons/fa';
+import { FaHome, FaUser, FaMoneyBill, FaFileInvoice, FaBook, FaTags } from 'react-icons/fa';
 
 export const headerOptions = [
-  { text: 'Inicio', icon: <FaHome /> },
-  { text: 'Clientes', icon: <FaUser /> },
-  { text: 'Gastos', icon: <FaMoneyBill /> },
-  { text: 'Condiciones de IVA', icon: <FaFileInvoice /> }
+  { text: 'Inicio', icon: <FaHome />, path: '/' },
+  { text: 'Clientes', icon: <FaUser />, path: '/client/' },
+  { text: 'Gastos', icon: <FaMoneyBill />, path: '/expense/' },
+  {
+    text: 'Catálogos',
+    icon: <FaBook />,
+    children: [
+      { text: 'Condiciones de IVA', icon: <FaFileInvoice />, path: '/condition-iva-type' },
+      { text: 'Categorías', icon: <FaTags />, path: '/category' },
+    ],
+  },
 ];
