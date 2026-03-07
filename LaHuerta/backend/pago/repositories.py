69019233ae3daee7f2b27e921784b067846fd437
Pablo Antúnez <pablooantunez@hotmail.com>
2026-03-07 14:19:43 -0300

    refactor: delete pago app

--- LaHuerta/backend/pago/repositories.py
+++
@@ -1,7 +0,0 @@
-from .models import Pago
-from .interfaces import IPaymentRepository
-
-class PaymentRepository(IPaymentRepository):
-    
-    def get_all_payments(self):
-        return Pago.objects.all()
